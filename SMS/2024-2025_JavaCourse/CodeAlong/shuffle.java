a5469b4417c3c48d020abe44cdd501da5eab713d
Dingyi-Ling <dingyi.ling.0205@gmail.com>
2025-02-19 16:57:01 +0000

    modified:   SMS/2024-2025_JavaCourse/CodeAlong/shuffle.java

--- SMS/2024-2025_JavaCourse/CodeAlong/shuffle.java
+++ SMS/2024-2025_JavaCourse/CodeAlong/shuffle.java
@@ -1,12 +1,12 @@
 public class shuffle {
     public static void main(String Args[]){
         int[] array = new int[10];
-        array = shuffle(array);
+        array = shuffleArray(array);
         for(int num: array){
             System.out.println(num);
         }
     }
-    public static int[] shuffle(int[] array){
+    public static int[] shuffleArray(int[] array){
         for(int i = 0; i < array.length; i++){
             int randomIndex = (int)(Math.random() * array.length);
             int temp = array[i];
