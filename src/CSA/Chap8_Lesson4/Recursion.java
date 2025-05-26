870c89074b66eb3f47bebef53c6e0831c2c1b74c
Dingyi-Ling <dingyi.ling.0205@gmail.com>
2025-05-26 09:52:13 -0400

    Fix base cases in recursion examples

--- src/CSA/Chap8_Lesson4/Recursion.java
+++ src/CSA/Chap8_Lesson4/Recursion.java
@@ -7,15 +7,15 @@ public class Recursion {
         System.out.println(x);
     }
     public int factorial(int n){
-        if(n == 1){
+        if(n <= 1){
             return 1;
         }
         return n * factorial(n-1);
     }
     public int fibonacci(int n){
-        if(n == 3){
-            return 1;
-        }else if(n == 2){
+        if(n <= 0){
+            return 0;
+        }else if(n == 1 || n == 2){
             return 1;
         }
         return fibonacci(n-1) + fibonacci(n-2);
